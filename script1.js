8f7a654e816d7c428e496410c13f25e7da831d14
maumau <mauricioramonsilvasandoval@gmail.com>
2023-04-29 17:32:57 -0600

    agregamos botones de pausa y reanudar para controlar el tiempo de las transiciones

--- script1.js
+++ script1.js
@@ -1,6 +1,4 @@
 var slideIndex = 1;
-var timer;
-
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -14,21 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+      console.log(slides[i]);
   }
   slides[slideIndex-1].style.display = "block";
-  timer = setTimeout(function() {
+  setTimeout(function() {
     showSlides(slideIndex += 1);
-  }, 2000); // tiempo en milisegundos (5 segundos)
-}
-
-function pauseSlides() {
-  clearTimeout(timer);
-}
-
-function resumeSlides() {
-  showSlides(slideIndex);
+  }, 3000); // tiempo en milisegundos (5 segundos)
 }
