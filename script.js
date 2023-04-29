b62f34a53b3568507e8789e3fe5213d652e82242
maumau <mauricioramonsilvasandoval@gmail.com>
2023-04-29 16:13:40 -0600

    cambiamos el script para agregar tiempo, y agregamos un @media queri al css para que funcione en celular con un marco gradiente, y no en omputadora

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -10,21 +10,17 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      slides[i].style.display = "none";
+      console.log(slides[i]);
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  setTimeout(function() {
+    showSlides(slideIndex += 1);
+  }, 3000); // tiempo en milisegundos (5 segundos)
 }
